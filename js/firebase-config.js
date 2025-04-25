c80aaca79e4df62629ba7fd705497f91b05de865
YangKaiChun <94584278+yangkaichun@users.noreply.github.com>
2025-04-25 23:21:18 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,14 @@
 // Replace with your Firebase config
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "…",
-  appId: "…"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBTBS1JpLm-4rlPuW6wZ38bnSG-oQPZPys",
+    authDomain: "health-education-system.firebaseapp.com",
+    databaseURL: "https://health-education-system-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "health-education-system",
+    storageBucket: "health-education-system.firebasestorage.app",
+    messagingSenderId: "535806176556",
+    appId: "1:535806176556:web:4b0ef588432eccc5a99089",
+    measurementId: "G-F2MEFR30M6"
+  };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 firebase.auth().signInAnonymously();
